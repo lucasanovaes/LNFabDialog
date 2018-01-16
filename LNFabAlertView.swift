3263f7440f52a3fa00e1174eb1198e4ff6c1764a
Lucas Novaes <lanovaes1@gmail.com>
2018-01-16 16:06:10 -0200

    Refactor name from LNFabItemAlertView to LNFabAlertView

---
+++ LNFabAlertView.swift
@@ -0,0 +1,14 @@
+//
+//  LNFabItemAlertView.swift
+//  LNFabDialog
+//
+//  Created by iCasei Site on 15/01/2018.
+//  Copyright © 2018 Lucas Novaes. All rights reserved.
+//
+
+import UIKit
+
+open class LNFabAlertView: UIView{
+    
+    
+}

91a08c9f19af9bf40239d21ab9189e2ae2c4cf36
Lucas Novaes <lanovaes1@gmail.com>
2018-01-16 16:07:22 -0200

    Struct that is gonna used to configure tableView Cell Layout

---
+++ LNFabItemLayout.swift
@@ -0,0 +1,21 @@
+//
+//  LNFabItemLayout.swift
+//  LNFabDialog
+//
+//  Created by iCasei Site on 15/01/2018.
+//  Copyright © 2018 Lucas Novaes. All rights reserved.
+//
+
+import Foundation
+
+public struct LNFabItemLayout{
+    
+    var font: UIFont
+    var textColor: UIColor
+    
+    init(font: UIFont, textColor: UIColor){
+        self.font = font
+        self.textColor = textColor
+    }
+    
+}

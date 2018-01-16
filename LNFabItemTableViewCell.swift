4fe1311b49e98dcd5c332f9cb9700cecde13671f
Lucas Novaes <lanovaes1@gmail.com>
2018-01-16 16:08:19 -0200

    Initial layout of tableView alert item, with constraints

---
+++ LNFabItemTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  LNFabItemTableViewCell.swift
+//  LNFabDialog
+//
+//  Created by iCasei Site on 15/01/2018.
+//  Copyright © 2018 Lucas Novaes. All rights reserved.
+//
+
+import UIKit
+
+class LNFabItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+
+}

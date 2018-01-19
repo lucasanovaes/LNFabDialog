c3f4bff182a059d35d5d023fc013ce4863404beb
Lucas Novaes <lanovaes1@gmail.com>
2018-01-19 14:29:17 -0200

    Release 1.2.3

--- LNFabDialog.podspec
+++ LNFabDialog.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'LNFabDialog'
-  s.version          = '1.2.0'
+  s.version          = '1.2.2'
   s.summary          = 'Simple dialog alert from Material Float Action Button'
 
 # This description is used to generate tags and improve search results.
--- LNFabDialog/Classes/LNFabItemTableViewCell.swift
+++ LNFabDialog/Classes/LNFabItemTableViewCell.swift
@@ -32,15 +32,16 @@ internal class LNFabItemTableViewCell: UITableViewCell {
             icon.image = model.image
         }
         
-        guard model.imageUrl != nil else {
-            activityIndicator.isHidden = true
-            return
-        }
-        
         if model.image == nil && model.imageUrl == nil{
             if model.placeholderImage != nil{
                 self.icon.image = model.placeholderImage!
             }
+        }
+        
+        guard model.imageUrl != nil else {
+            activityIndicator.isHidden = true
+            activityIndicator.stopAnimating()
+            return
         }
         
         URLSession.shared.dataTask(with: model.imageUrl!) { [weak self] (data, response, error) in
@@ -48,6 +49,7 @@ internal class LNFabItemTableViewCell: UITableViewCell {
                 DispatchQueue.main.async {
                     self?.icon.image = UIImage(data: data!)
                     self?.activityIndicator.isHidden = true
+                    self?.activityIndicator.stopAnimating()
                 }
             }
         }.resume()
